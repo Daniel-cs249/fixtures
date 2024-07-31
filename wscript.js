e3cf773141f486cb1a4352e3b584d499acf338e0
Daniel-cs249 <daniel.cs249@gmail.com>
2024-07-31 12:29:08 +0530

    error in script

--- wscript.js
+++ wscript.js
@@ -1,6 +1,6 @@
 function login()
 {
-    Event.preventDefault()
+    event.preventDefault()
     console.log("submitted")
     window.location.replace("http://127.0.0.1:5500/fixtures/namentry.html")
 }
