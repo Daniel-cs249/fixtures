da36c7052e5b7298ecd301eafb6a67ab087f6376
Daniel-cs249 <daniel.cs249@gmail.com>
2024-08-01 10:51:32 +0530

    design

--- wscript.js
+++ wscript.js
@@ -1,6 +1,21 @@
 function login()
 {
-    event.preventDefault()
-    console.log("submitted")
-    window.location.replace("http://127.0.0.1:5500/fixtures/namentry.html")
+    
+
+    const username=document.getElementById("username").value
+    const password=document.getElementById("password").value
+    
+    if(!username && !password)
+    {
+        alert("enter the folowing details")
+        return false
+    }
+    if(username=="danielcs" && password=="ishowlag")
+    {
+        window.location.href="http://127.0.0.1:5500/fixtures/namentry.html"
+    }
+    else{
+        alert("invalid username or password")
+    }
+    
 }
